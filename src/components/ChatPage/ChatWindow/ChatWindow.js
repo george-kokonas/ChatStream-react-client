35120bb0e53854c694764b33db29b11619d99231
George Kokonas <g.kokwnas@gmail.com>
2023-05-05 10:12:25 +0300

    add initial elements and for the messaging window

--- src/components/ChatPage/ChatWindow/ChatWindow.js
+++ src/components/ChatPage/ChatWindow/ChatWindow.js
@@ -7,14 +7,24 @@ const ChatWindow = () => {
       <Topbar />
       <div className='chatContainer'>
         <div className='chatMenu'>
-          <div className='wrapper-menu'> menu</div>
+          <div className='wrapper-menu'>menu</div>
         </div>
         <div className='message-window'>
           <div className='wrapper-message-window'>
             <div className='message'>
               <Message myMessage={true}></Message>
               <Message></Message>
+              <Message myMessage={true}></Message>
               <Message></Message>
+              <Message myMessage={true}></Message>
+            </div>
+            <div className='wrapper-input'>
+              <input
+                className='userMessage-Input'
+                type='text'
+                placeholder='enter your message'
+              ></input>
+              <button className='sendMessage-Btn'>send</button>
             </div>
           </div>
         </div>
