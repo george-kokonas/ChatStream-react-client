19b45ee954cb39d6b6735cead4539a096ed738ee
George Kokonas <g.kokwnas@gmail.com>
2023-05-08 23:24:39 +0300

    fetch the receiver of each chatroom that the logged user participates and render the email on the rooms column

--- src/components/ChatPage/ChatWindow/ChatWindow.js
+++ src/components/ChatPage/ChatWindow/ChatWindow.js
@@ -1,30 +1,46 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import Topbar from "../Topbar/Topbar";
 import Rooms from "../Rooms/Rooms";
 import "./chatWindow.css";
-const ChatWindow = () => {  
-  const [rooms,setRooms] = useState([]);
+const ChatWindow = () => {
+  const [rooms, setRooms] = useState([]);
   const user = JSON.parse(localStorage.getItem("user"));
 
-  useEffect(()=>{
-    const getRooms = async() =>{
-      const {data}= await axios.get(`http://localhost:8000/chat/getChatRoom/${user._id}`);
-      setRooms(data)
-    }
-    getRooms()
-  },[user._id])
+  useEffect(() => {
+    const getRooms = async () => {
+      try {
+        const { data } = await axios.get(
+          `http://localhost:8000/chat/getChatRoom/${user._id}`
+        );
+        setRooms(data);
+      } catch (error) {
+        //add decent error handling
+        console.log(error);
+        alert("Error fetching data");
+      }
+    };
+    getRooms();
+  }, [user._id]);
+
   return (
     <>
       <Topbar />
       <div className='chatContainer'>
+        {/* LEFT BAR */}
         <div className='chatMenu'>
-          <div className='wrapper-menu'>menu</div>
+          <div className='wrapper-menu'>
+            ---------ROOMS--------
+            {rooms.map((room) => (
+              <Rooms chatroom={room} loggedUser={user} key={room._id} />
+            ))}
+          </div>
         </div>
+
+        {/* MAIN PAGE */}
         <div className='message-window'>
           <div className='wrapper-message-window'>
-            <div className='message'>
-            </div>
+            <div className='message'>------MESSAGES-------</div>
             <div className='wrapper-input'>
               <input
                 className='userMessage-Input'
@@ -35,8 +51,10 @@ const ChatWindow = () => {
             </div>
           </div>
         </div>
+
+        {/* RIGHT PART */}
         <div className='onlineUsers'>
-          <div className='wrapper-onlineUsers'>online users</div>
+          <div className='wrapper-onlineUsers'>----ONLINE USERS---------</div>
         </div>
       </div>
     </>
--- src/components/ChatPage/Rooms/Rooms.js
+++ src/components/ChatPage/Rooms/Rooms.js
@@ -1,15 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./rooms.css";
 
-const Rooms = ({ myMessage }) => {
-  return (
-    <div className={myMessage ? "message isMine" : "message"}>
-      <div className='container-message'>
-        {/* messages goes here */}
-        <p>icon</p>
-        <p className='messageText'>Hello man</p>
-      </div>
-    </div>
-  );
+const Rooms = ({ chatroom, loggedUser }) => {
+  const [receiver, setReceiver] = useState({});
+  const { members } = chatroom;
+  const { _id: loggedUserId } = loggedUser;
+
+  // console.log(loggedUserId);
+
+  useEffect(() => {
+    const receiverId = members.filter((memberId) => memberId !== loggedUserId);
+
+    const getReceiver = async () => {
+      try {
+        const { data } = await axios.get(
+          `http://localhost:8000/user/getUser/${receiverId}`
+        );
+        setReceiver(data);
+      } catch (error) {
+        console.log(error);
+        alert("Error fetching users...");
+      }
+    };
+    getReceiver();
+  }, [chatroom, loggedUser, loggedUserId, members]);
+
+  return <div className='room-container'>{receiver.email}</div>;
 };
 
 export default Rooms;
