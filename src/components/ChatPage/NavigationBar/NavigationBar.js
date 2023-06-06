584b986e8f852eac3cb1d2001883cd1181eb7ec2
George Kokonas <g.kokwnas@gmail.com>
2023-06-07 00:11:16 +0300

    add sound that is produced when navbar notification appears. add styling to homepage and chatpage

--- src/components/ChatPage/NavigationBar/NavigationBar.js
+++ src/components/ChatPage/NavigationBar/NavigationBar.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Container from "react-bootstrap/Container";
@@ -8,6 +9,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMessage } from "@fortawesome/free-solid-svg-icons";
 import defaultUserIcon from "../../../assets/defaultUserIcon.png";
 import "./navigationBar.css";
+
+import sound from "../../../assets/newUnreadSound.wav";
 
 import {
   MDBNavbarItem,
@@ -27,6 +30,7 @@ const NavigationBar = ({
   socket,
 }) => {
   const navigate = useNavigate();
+  const audioRef = useRef(null);
 
   const logoutHandler = () => {
     localStorage.removeItem("token");
@@ -36,6 +40,18 @@ const NavigationBar = ({
     navigate("/");
   };
 
+  const playSound = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
+  useEffect(() => {
+    if (navUnreadMessages) {
+      playSound();
+    }
+  }, [navUnreadMessages]);
+
   return (
     <>
       <Navbar className='navbar'>
@@ -43,14 +59,17 @@ const NavigationBar = ({
           <Navbar.Brand id='navbar-logo'>ChatStream</Navbar.Brand>
 
           {navUnreadMessages && (
-            <FontAwesomeIcon
-              className='unread-msg-icon'
-              icon={faMessage}
-              size='xl'
-              bounce
-            />
+            <>
+              <FontAwesomeIcon
+                className='unread-msg-icon'
+                icon={faMessage}
+                size='xl'
+                bounce
+              />
+              <audio ref={audioRef} src={sound} />
+            </>
           )}
-          <Nav className='d-flex flex-row mb-0'>
+          <Nav className=''>
             <Navbar.Brand id='navbar-username'>{`Hello, ${currentUser?.username}!`}</Navbar.Brand>
 
             <MDBNavbarItem>
@@ -63,7 +82,7 @@ const NavigationBar = ({
                   <img
                     src={currentUser?.profileImage || defaultUserIcon}
                     alt='avatar'
-                    className='rounded-circle d-flex align-self-center me-1 shadow-1-strong'
+                    className='rounded-circle me-1 shadow-1-strong'
                     width='32'
                     height='32'
                     loading='lazy'
