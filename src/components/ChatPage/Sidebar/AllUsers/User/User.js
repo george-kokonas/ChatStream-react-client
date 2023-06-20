5c5995fbf11ec200d343ec47c97dcdf2efd8952a
George Kokonas <g.kokwnas@gmail.com>
2023-06-20 17:21:40 +0300

    in users tab, add online status at the  avatar border instead of rigth side of the card

--- src/components/ChatPage/Sidebar/AllUsers/User/User.js
+++ src/components/ChatPage/Sidebar/AllUsers/User/User.js
@@ -1,35 +1,41 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle } from "@fortawesome/free-solid-svg-icons";
+"use client";
 import defaultAvatar from "../../../../../assets/defaultAvatar.png";
 import styles from "./User.module.scss";
+import { MDBBadge } from "mdb-react-ui-kit";
 
 const User = ({ user, isOnline, newRoomHandler }) => {
   return (
-      <li
-        className={`${styles.userItem}`}
-        onClick={() => newRoomHandler(user)}
-      >
-      
-          <div className={styles.leftSide}>
+    <>
+      <li className={`${styles.userItem}`} onClick={() => newRoomHandler(user)}>
+
+        {/* PROFILE AVATAR */}
+        <div className={styles.avatar}>
+          <div className={styles.image}>
             <img
               src={user.profileImage || defaultAvatar}
               alt='avatar'
-              className='rounded-circle d-flex align-self-center  me-3 shadow-1-strong'
+              className='rounded-circle shadow-1-strong'
               width='55'
             />
+            <div className={styles.status}>
+              <MDBBadge
+                className={`${
+                  isOnline ? "bg-success" : "bg-danger"
+                } p-2 border border-light rounded-circle`}
+              >
+                <span className='visually-hidden'>New alerts</span>
+              </MDBBadge>
             </div>
-            <div className={styles.center}>
-              <div className={styles.username}>{user.username}</div>
-              <div className={styles.profileInfo}>{user.profileInfo}</div>
-            </div>
-
-          <div className={styles.rightSide}>
-            <FontAwesomeIcon
-              icon={faCircle}
-              style={isOnline ? { color: "green" } : { color: "#AA0000" }}
-            />{" "}
           </div>
+        </div>
+
+        {/* PROFILE USERNAME AND INFO */}
+        <div className={styles.center}>
+          <div className={styles.username}>{user.username}</div>
+          <div className={styles.profileInfo}>{user.profileInfo}</div>
+        </div>
       </li>
+    </>
   );
 };
 
